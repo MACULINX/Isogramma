d470be9dbf958cd412f62649920709a2435455ff
Marco <testrco4560@gmail.com>
2023-05-09 19:37:35 +0200

    First upload

---
+++ bucci.marco.3i.isogramma/Isogramma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+
+        word = word.ToLower();
+
+        for (int i = 0; i < word.Length; i++)
+            for (int j = i+1; j < word.Length; j++)
+                if( (word[i] == word[j] ) && ( word[i] != ' ' ) && ( word[i] != '-' ))
+                    return false;
+
+        return true;
+    }
+
+}
